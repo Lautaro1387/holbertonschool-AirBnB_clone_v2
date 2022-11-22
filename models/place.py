a8bd6e3307f9014242e00eef1b07ca677ec7360b
root <root@H45.hbtn.io>
2022-11-22 15:36:50 -0300

    Task 7

--- models/place.py
+++ models/place.py
@@ -8,10 +8,8 @@ class Place(BaseModel, Base):
     """A place to stay"""
     __tablename__ = 'places'
     city_id = Column(String(60), ForeignKey("cities.id"), nullable=False)
+    user_id = Column(String(60))
     name = Column(String(128), nullable=False)
-    city_id = ""
-    user_id = ""
-    name = ""
     description = ""
     number_rooms = 0
     number_bathrooms = 0
