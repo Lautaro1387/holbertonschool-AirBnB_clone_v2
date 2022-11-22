dce08fb0ef1bf0d0d33da43a3310b16637ed780c
gallit0 <mateogesede@gmail.com>
2022-11-22 14:48:03 -0300

    WIP: task 6

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -9,7 +9,7 @@ class DBStorage:
     __engine = None
     __session = None
 
-    __init__(self):
+    def __init__(self):
         self.__engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
                                 "hbnb_dev", ,),
                                 pool_pre_ping=True)
