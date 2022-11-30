5df27076c3540204bd2a24bd8d1109288c8adb8d
Lautaro1308 <lautaro12387@gmail.com>
2022-11-29 21:25:33 -0300

    Fix pycodestyle

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,20 +1,24 @@
 #!/usr/bin/python3
 """Starts a Flask web application"""
-from flask import Flask
 
+from flask import Flask
 app = Flask(__name__)
+
 
 @app.route('/', strict_slashes=False)
 def index():
     return f'Hello HBNB!'
 
-@app.route('/hbnb', strict_slashes=False)
-def hbnb():
-    return f'HBNB'
 
 @app.route('/c/<text>', strict_slashes=False)
 def c_text():
     return f'C <text>'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    return f'HBNB'
+
 
 if __name__ == '__main__':
     app.run(debug=Flase, host=0.0.0.0, port=5000)
