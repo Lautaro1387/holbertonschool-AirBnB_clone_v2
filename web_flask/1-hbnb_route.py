940e78dd2c91e5f3fb7f385184ba7db430f4b6d1
Lautaro1308 <lautaro12387@gmail.com>
2022-11-30 09:36:52 -0300

    Fix pycodestyle

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,19 +1,23 @@
 #!/usr/bin/python3
-"""Starts a Flask web application"""
+""" Script that starts Flask web application
+    Return: Hello HBNB!, HBNB
+"""
+
 
 from flask import Flask
+
+
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
 def index():
-    return f'Hello HBNB!'
+    return 'Hello HBNB!'
 
 
-@app.route('/hbnb', strict_slashes=False)
+@app.route('', strict_slashes=False)
 def hbnb():
-    return f'HBNB'
-
+    return 'HBNB'
 
 if __name__ == '__main__':
-    app.run(debug=Flase, host=0.0.0.0, port=5000)
+    app.run(host='0.0.0.0', port=5000)
