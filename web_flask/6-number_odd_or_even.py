818927f646bedb188c34c258791a809b37d27f31
Lautaro1308 <lautaro12387@gmail.com>
2022-11-30 19:29:23 -0300

    Task 6

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -6,6 +6,7 @@
             Python is cool
             <int> is a number
             Number: <n>
+            Number: <n> is even|odd
 """
 
 
@@ -45,6 +46,11 @@ def number(n):
 @app.route('/number_template/<int:n>', strict_slashes=False)
 def number_template(n):
     return render_template('5-number.html', n=n)
+
+
+@app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
+def number_odd_or_even(n):
+    return render_template('6-number_odd_or_even.html', n=n)
 
 
 if __name__ == '__main__':
