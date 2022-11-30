7c633e393feaf5665c72a27c95f9956d90d51cf8
Lautaro1308 <lautaro12387@gmail.com>
2022-11-30 09:31:17 -0300

    Fix pycodestyle

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-""" A script that starts Flask web application
-    Return:
-        Hello HBNB!
+""" Script that starts Flask web application
+    Return: Hello HBNB!
 """
 
 
@@ -9,11 +8,9 @@ from flask import Flask
 
 
 app = Flask(__name__)
-
-
 @app.route('/', strict_slashes=False)
 def hello():
-    return 'Hello HBNB!'
+    return f'Hello HBNB!'
 
 
 if __name__ == '__main__':
