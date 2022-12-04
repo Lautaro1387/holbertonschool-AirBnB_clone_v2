d78c8b6f60bdf1a1e01710f6964eb1674ceb2d3a
Lautaro1308 <lautaro12387@gmail.com>
2022-12-04 19:50:39 -0300

    Task 8

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Script that starts a Flask web application"""
+
+
+from flask import Flask, render_template
+from markupsafe import escape
+from models import storage
+from models.state import State
+
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def storage_close():
+    storage.close()
+
+
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    states = storage.all(Storage)
+    all_st = []
+    for state in states.values():
+        all_st.append([state.id, state.name])
+    return render_template('7-states_list.html', states=all_st)
+
+if __name == '__main__':
+    app.run(host='0.0.0.0', port='5000')
